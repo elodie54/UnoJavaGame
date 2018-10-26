76c991c3b1c7fe3cad94d53ab518c7e4b9d1325b
elodie54 <44501028+elodie54@users.noreply.github.com>
2018-10-26 16:22:19 +0200

    V3

--- src/Instance/Jeu.java
+++ src/Instance/Jeu.java
@@ -1,5 +1,36 @@
 package Instance;
 
-public class Jeu {
+import java.util.List;
 
+import ClasseAbstraite.Carte;
+
+public class Jeu {
+	private static final Jeu INSTANCE = new Jeu();
+	private List<Carte> carteDisponible;
+
+	public static Jeu getInstance() {
+		return INSTANCE;
+	}
+
+	private Jeu() {
+		/*
+		 * 19 cartes rouges numérotées de 0 à 9 (le 0 en un seul exemplaire) ; 19 cartes
+		 * vertes numérotées de 0 à 9 (le 0 en un seul exemplaire) ; 19 cartes jaunes
+		 * numérotées de 0 à 9 (le 0 en un seul exemplaire) ; 19 cartes bleues
+		 * numérotées de 0 à 9 (le 0 en un seul exemplaire) ; 8 cartes +2 (2 de chaque
+		 * couleur : rouge, vert, jaune, bleu) ; 8 cartes inversion (2 de chaque couleur
+		 * : rouge, vert, jaune, bleu) ; 8 cartes « passer / passe ton tour » (2 de
+		 * chaque couleur : rouge, vert, jaune, bleu) ; 4 cartes Joker ; 4 cartes « +4 »
+		 * ou « Super Joker ».
+		 */
+		
+		for (int i = 0; i < 19; i++) {
+
+		}
+
+	}
+
+	public Carte distribuer() {
+		return null;
+	}
 }
